425f0ec579b032adea9a6b0977f9e617fcb61a41
priyesh kumar <59879071+priyeshsingh535@users.noreply.github.com>
2026-06-14 17:20:21 +0530

    crud with H2 db

---
+++ src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/entities/EmployeeEntity.java
@@ -0,0 +1,30 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Integer age;
+
+    private LocalDate dateOfJoining;
+
+    private Boolean isActive;
+}
